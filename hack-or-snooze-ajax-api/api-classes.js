250060686b34c74bb2c5145e75f1534da360e5b8
Thamiris Zalkowitsch <thamizalko@gmail.com>
2020-12-22 19:44:50 -0800

    Erase and start over

--- hack-or-snooze-ajax-api/api-classes.js
+++ hack-or-snooze-ajax-api/api-classes.js
@@ -27,7 +27,7 @@ class StoryList {
     // query the /stories endpoint (no auth required)
     const response = await axios.get(`${BASE_URL}/stories`);
 
-    // turn plain old story objects from API into instances of Story class
+    // turn the plain old story objects from the API into instances of the Story class
     const stories = response.data.stories.map(story => new Story(story));
 
     // build an instance of our own class using the new array of stories
@@ -44,52 +44,12 @@ class StoryList {
    */
 
   async addStory(user, newStory) {
-    const response = await axios({
-      method: "POST",
-      url: `${BASE_URL}/stories`,
-      data: {
-        // request body
-        // this is the format specified by the API
-        token: user.loginToken,
-        story: newStory,
-      }
-    });
-
-    // make a Story instance out of the story object we get back
-    newStory = new Story(response.data.story);
-    // add the story to the beginning of the list
-    this.stories.unshift(newStory);
-    // add the story to the beginning of the user's list
-    user.ownStories.unshift(newStory);
-
-    return newStory;
-  }
-
-  /**
-   * Method to make a DELETE request to remove a particular story
-   *  and also update the StoryList
-   *
-   * - user: the current User instance
-   * - storyId: the ID of the story you want to remove
-   */
-
-  async removeStory(user, storyId) {
-    await axios({
-      url: `${BASE_URL}/stories/${storyId}`,
-      method: "DELETE",
-      data: {
-        token: user.loginToken
-      },
-    });
-
-    // filter out the story whose ID we are removing
-    this.stories = this.stories.filter(story => story.storyId !== storyId);
-
-    // do the same thing for the user's list of stories
-    user.ownStories = user.ownStories.filter(s => s.storyId !== storyId
-    );
+    // TODO - Implement this functions!
+    // this function should return the newly created story so it can be used in
+    // the script.js file where it will be appended to the DOM
   }
 }
+
 
 /**
  * The User class to primarily represent the current user.
@@ -123,7 +83,7 @@ class User {
       user: {
         username,
         password,
-        name,
+        name
       }
     });
 
@@ -146,7 +106,7 @@ class User {
     const response = await axios.post(`${BASE_URL}/login`, {
       user: {
         username,
-        password,
+        password
       }
     });
 
@@ -175,7 +135,9 @@ class User {
 
     // call the API
     const response = await axios.get(`${BASE_URL}/users/${username}`, {
-      params: {token}
+      params: {
+        token
+      }
     });
 
     // instantiate the user from the API information
@@ -187,106 +149,7 @@ class User {
     // instantiate Story instances for the user's favorites and ownStories
     existingUser.favorites = response.data.user.favorites.map(s => new Story(s));
     existingUser.ownStories = response.data.user.stories.map(s => new Story(s));
-
     return existingUser;
-  }
-
-  /**
-   * This function fetches user information from the API
-   *  at /users/{username} using a token. Then it sets all the
-   *  appropriate instance properties from the response with the current user instance.
-   */
-
-  async retrieveDetails() {
-    const response = await axios.get(`${BASE_URL}/users/${this.username}`, {
-      params: {
-        token: this.loginToken
-      }
-    });
-
-    // update all of the user's properties from the API response
-    this.name = response.data.user.name;
-    this.createdAt = response.data.user.createdAt;
-    this.updatedAt = response.data.user.updatedAt;
-
-    // remember to convert the user's favorites and ownStories into instances of Story
-    this.favorites = response.data.user.favorites.map(s => new Story(s));
-    this.ownStories = response.data.user.stories.map(s => new Story(s));
-
-    return this;
-  }
-
-  /**
-   * Add a story to the list of user favorites and update the API
-   * - storyId: an ID of a story to add to favorites
-   */
-
-  addFavorite(storyId) {
-    return this._toggleFavorite(storyId, "POST");
-  }
-
-  /**
-   * Remove a story to the list of user favorites and update the API
-   * - storyId: an ID of a story to remove from favorites
-   */
-
-  removeFavorite(storyId) {
-    return this._toggleFavorite(storyId, "DELETE");
-  }
-
-  /**
-   * A helper method to either POST or DELETE to the API
-   * - storyId: an ID of a story to remove from favorites
-   * - httpVerb: POST or DELETE based on adding or removing
-   */
-  async _toggleFavorite(storyId, httpVerb) {
-    await axios({
-      url: `${BASE_URL}/users/${this.username}/favorites/${storyId}`,
-      method: httpVerb,
-      data: {
-        token: this.loginToken
-      }
-    });
-
-    await this.retrieveDetails();
-    return this;
-  }
-
-  /**
-   * Send a PATCH request to the API in order to update the user
-   * - userData: the user properties you want to update
-   */
-
-  async update(userData) {
-    const response = await axios({
-      url: `${BASE_URL}/users/${this.username}`,
-      method: "PATCH",
-      data: {
-        user: userData,
-        token: this.loginToken
-      }
-    });
-
-    // "name" is really the only property you can update
-    this.name = response.data.user.name;
-
-    // Note: you can also update "password" but we're not storing it
-    return this;
-  }
-
-  /**
-   * Send a DELETE request to the API in order to remove the user
-   */
-
-  async remove() {
-    // this function is really just a wrapper around axios
-    await axios({
-      url: `${BASE_URL}/users/${this.username}`,
-      method: "DELETE",
-      data: {
-        token: this.loginToken
-      }
-    });
   }
 }
 
@@ -309,33 +172,5 @@ class Story {
     this.storyId = storyObj.storyId;
     this.createdAt = storyObj.createdAt;
     this.updatedAt = storyObj.updatedAt;
-  }
-
-  /**
-   * Make a PATCH request against /stories/{storyID} to update a single story
-   * - user: an instance of User
-   * - storyData: an object containing the properties you want to update
-   */
-
-  async update(user, storyData) {
-    const response = await axios({
-      url: `${BASE_URL}/stories/${this.storyId}`,
-      method: "PATCH",
-      data: {
-        token: user.loginToken,
-        story: storyData
-      }
-    });
-
-    const { author, title, url, updatedAt } = response.data.story;
-
-    // these are the only fields that you can change with a PATCH update
-    //  so we don't need to worry about updating the others
-    this.author = author;
-    this.title = title;
-    this.url = url;
-    this.updatedAt = updatedAt;
-
-    return this;
   }
 }
---
+++ hack-or-snooze-ajax-api/ui.js
@@ -0,0 +1,217 @@
+$(async function() {
+  // cache some selectors we'll be using quite a bit
+  const $allStoriesList = $("#all-articles-list");
+  const $submitForm = $("#submit-form");
+  const $filteredArticles = $("#filtered-articles");
+  const $loginForm = $("#login-form");
+  const $createAccountForm = $("#create-account-form");
+  const $ownStories = $("#my-articles");
+  const $navLogin = $("#nav-login");
+  const $navLogOut = $("#nav-logout");
+
+  // global storyList variable
+  let storyList = null;
+
+  // global currentUser variable
+  let currentUser = null;
+
+  await checkIfLoggedIn();
+
+  /**
+   * Event listener for logging in.
+   *  If successfully we will setup the user instance
+   */
+
+  $loginForm.on("submit", async function(evt) {
+    evt.preventDefault(); // no page-refresh on submit
+
+    // grab the username and password
+    const username = $("#login-username").val();
+    const password = $("#login-password").val();
+
+    // call the login static method to build a user instance
+    const userInstance = await User.login(username, password);
+    // set the global user to the user instance
+    currentUser = userInstance;
+    syncCurrentUserToLocalStorage();
+    loginAndSubmitForm();
+  });
+
+  /**
+   * Event listener for signing up.
+   *  If successfully we will setup a new user instance
+   */
+
+  $createAccountForm.on("submit", async function(evt) {
+    evt.preventDefault(); // no page refresh
+
+    // grab the required fields
+    let name = $("#create-account-name").val();
+    let username = $("#create-account-username").val();
+    let password = $("#create-account-password").val();
+
+    // call the create method, which calls the API and then builds a new user instance
+    const newUser = await User.create(username, password, name);
+    currentUser = newUser;
+    syncCurrentUserToLocalStorage();
+    loginAndSubmitForm();
+  });
+
+  /**
+   * Log Out Functionality
+   */
+
+  $navLogOut.on("click", function() {
+    // empty out local storage
+    localStorage.clear();
+    // refresh the page, clearing memory
+    location.reload();
+  });
+
+  /**
+   * Event Handler for Clicking Login
+   */
+
+  $navLogin.on("click", function() {
+    // Show the Login and Create Account Forms
+    $loginForm.slideToggle();
+    $createAccountForm.slideToggle();
+    $allStoriesList.toggle();
+  });
+
+  /**
+   * Event handler for Navigation to Homepage
+   */
+
+  $("body").on("click", "#nav-all", async function() {
+    hideElements();
+    await generateStories();
+    $allStoriesList.show();
+  });
+
+  /**
+   * On page load, checks local storage to see if the user is already logged in.
+   * Renders page information accordingly.
+   */
+
+  async function checkIfLoggedIn() {
+    // let's see if we're logged in
+    const token = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
+
+    // if there is a token in localStorage, call User.getLoggedInUser
+    //  to get an instance of User with the right details
+    //  this is designed to run once, on page load
+    currentUser = await User.getLoggedInUser(token, username);
+    await generateStories();
+
+    if (currentUser) {
+      showNavForLoggedInUser();
+    }
+  }
+
+  /**
+   * A rendering function to run to reset the forms and hide the login info
+   */
+
+  function loginAndSubmitForm() {
+    // hide the forms for logging in and signing up
+    $loginForm.hide();
+    $createAccountForm.hide();
+
+    // reset those forms
+    $loginForm.trigger("reset");
+    $createAccountForm.trigger("reset");
+
+    // show the stories
+    $allStoriesList.show();
+
+    // update the navigation bar
+    showNavForLoggedInUser();
+  }
+
+  /**
+   * A rendering function to call the StoryList.getStories static method,
+   *  which will generate a storyListInstance. Then render it.
+   */
+
+  async function generateStories() {
+    // get an instance of StoryList
+    const storyListInstance = await StoryList.getStories();
+    // update our global variable
+    storyList = storyListInstance;
+    // empty out that part of the page
+    $allStoriesList.empty();
+
+    // loop through all of our stories and generate HTML for them
+    for (let story of storyList.stories) {
+      const result = generateStoryHTML(story);
+      $allStoriesList.append(result);
+    }
+  }
+
+  /**
+   * A function to render HTML for an individual Story instance
+   */
+
+  function generateStoryHTML(story) {
+    let hostName = getHostName(story.url);
+
+    // render story markup
+    const storyMarkup = $(`
+      <li id="${story.storyId}">
+        <a class="article-link" href="${story.url}" target="a_blank">
+          <strong>${story.title}</strong>
+        </a>
+        <small class="article-author">by ${story.author}</small>
+        <small class="article-hostname ${hostName}">(${hostName})</small>
+        <small class="article-username">posted by ${story.username}</small>
+      </li>
+    `);
+
+    return storyMarkup;
+  }
+
+  /* hide all elements in elementsArr */
+
+  function hideElements() {
+    const elementsArr = [
+      $submitForm,
+      $allStoriesList,
+      $filteredArticles,
+      $ownStories,
+      $loginForm,
+      $createAccountForm
+    ];
+    elementsArr.forEach($elem => $elem.hide());
+  }
+
+  function showNavForLoggedInUser() {
+    $navLogin.hide();
+    $navLogOut.show();
+  }
+
+  /* simple function to pull the hostname from a URL */
+
+  function getHostName(url) {
+    let hostName;
+    if (url.indexOf("://") > -1) {
+      hostName = url.split("/")[2];
+    } else {
+      hostName = url.split("/")[0];
+    }
+    if (hostName.slice(0, 4) === "www.") {
+      hostName = hostName.slice(4);
+    }
+    return hostName;
+  }
+
+  /* sync current user information to localStorage */
+
+  function syncCurrentUserToLocalStorage() {
+    if (currentUser) {
+      localStorage.setItem("token", currentUser.loginToken);
+      localStorage.setItem("username", currentUser.username);
+    }
+  }
+});
